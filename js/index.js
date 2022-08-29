8f5399ddea302a1d4c3ba0e02470a16f756b9138
szilagyipeter99 <60160988+szilagyipeter99@users.noreply.github.com>
2022-08-29 22:43:43 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,50 @@
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.navbar-list');
+
+
+menuButton.addEventListener('click', showMenu);
+
+
+function showMenu() {
+  menu.classList.toggle("responsive-active");
+  menuButton.classList.toggle("menu-button-active");
+}
+
+
+document.querySelector('.references .gallery-bullets').addEventListener('click', scrollGallery);
+
+let galleryWidth = 5*200;
+
+function scrollGallery() {
+
+  if (event.target.className == 'bullet') {
+    let allBullets = document.querySelectorAll('.references .gallery-bullets .bullet');
+
+    for (let i = 0; i < allBullets.length; i++) {
+      allBullets[i].style.background = "white";
+      allBullets[i].className = "bullet";
+    }
+
+
+    let index = Array.from(event.target.parentElement.children).indexOf(event.target);
+
+
+    document.querySelector('.references .references-gallery').style['margin-left'] = `${index * - galleryWidth}px`;
+    event.target.className = "bullet active";
+    event.target.style.background = "grey";
+
+  }
+
+}
+
+
+const modal = document.getElementById('modalWrap');
+
+function showInModal(e) {
+  modal.style.display = 'block';
+  modal.querySelector("img").setAttribute('src', e.getAttribute('src'));
+}
+
+function hideModal() {
+  modal.style.display = 'none';
+}
